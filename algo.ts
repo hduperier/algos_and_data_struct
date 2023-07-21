129d65d94c73dcc6557bc5fe2ba3598e463d9c3f
hduperier <59696097+hduperier@users.noreply.github.com>
2023-07-21 12:56:38 -0400

    Started Course

---
+++ algo.ts
@@ -0,0 +1,51 @@
+function sum_char_codes(n: string): number {
+    let sum = 0;
+    for (let i=0; i<n.length; ++i) {
+        // for loop has to execute length of the string
+        // if string grows by 50%, computation increases by 50%
+        // so therefore it grows linearly and for one more unit of string, theres one more loop for it to do
+        sum += n.charCodeAt(i);
+
+    }
+    return sum;
+}
+
+function sum_char_codes2(n: string): number {
+    let sum = 0;
+    for (let i=0; i<n.length; ++i) {
+        // for loop has to execute length of the string
+        // if string grows by 50%, computation increases by 50%
+        // so therefore it grows linearly and for one more unit of string, theres one more loop for it to do
+        sum += n.charCodeAt(i);
+    }
+
+    for (let i=0; i<n.length; ++i) {
+        sum += n.charCodeAt(i);
+        //so now we added the same function again
+        // time complexity is still O(N) because constants are dropped
+        // so O(2*N) == O(N)
+    }
+
+    return sum;
+}
+
+function sum_char_codes3(n: string): number {
+    let sum = 0;
+    for (let i=0; i<n.length; ++i) {
+        // for loop has to execute length of the string
+        // if string grows by 50%, computation increases by 50%
+        // so therefore it grows linearly and for one more unit of string, theres one more loop for it to do
+        const charCode = n.charCodeAt(i);
+        // Looking for capital E
+        if (charCode === 69) {
+            return sum;
+            // so theoretically, this could end early and be O(N-i), but speaking in time complexity
+            // we generally consider worst-case, so if the run doesnt end early because there's no capital E, 
+            // then the worst case time complexity would be O(N) for this function as well
+        }
+        sum+= charCode;
+
+    }
+    return sum;
+}
+
